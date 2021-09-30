2f791c8cd7d71ed559e5b3ba83b6db9f3fdb5003
Eu Jeong Hwang <59908525+hj2bf@users.noreply.github.com>
2021-09-30 23:31:56 +0900

    initial commit

---
+++ mysql.js
@@ -0,0 +1,2 @@
+//always use the beneath code when starting mysql in cmd 
+//docker run --rm -p 3306:3306 --name test-db -e MYSQL_ROOT_PASSWORD=1234 mysql:5.7 mysqld --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci;
